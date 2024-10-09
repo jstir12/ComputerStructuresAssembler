db8a212d3251964bdc57615e43bd1123bb0a2148
Jacob Stir <jacob27stir@gmail.com>
2024-10-09 18:13:58 -0400

    Implemted the Optable class and created a main.py file.

---
+++ main.py
@@ -0,0 +1,8 @@
+from OpTable import OpTable
+
+""" Used to test Optable.py
+objectCode = OpTable()
+print (objectCode.getOpcode("FIX")) # Returns ('C4', '1')
+print (objectCode.getFormat("FIX")) # Returns 1
+print (objectCode.getOpcodeValue("FIX")) # Returns C4
+"""
