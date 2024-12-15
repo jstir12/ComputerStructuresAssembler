1f2e970de24f212f169e506a9663613d380d901f
Jacob Stir <jacob27stir@gmail.com>
2024-12-15 13:17:52 -0500

    Update file upload prompt to accept file name and adjust file path in PassOne execution

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def main():
     machine_code = []
     # Prompt the user for the file path
     
-    #file_path = input("Please enter the path to the file you want to upload: ")
+    file_path = input("Please enter the name of the file you want to upload: ")
+    
     # Create the OpTable and SymTable objects
     op_table = OpTable()
     # Create the PassOne object
     passOne = PassOne(op_table)
-    control_sections = passOne.run("Assembly/control_section.txt") 
+    control_sections = passOne.run(f'Assembly/{file_path}.txt') 
     # Create the PassTwo object
     passTwo = PassTwo(control_sections, op_table)
     passTwo.generate_machine_code()
